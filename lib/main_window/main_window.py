15906da5740ca2ac65814348caf6bbbc992e3a4c
Hyeonki Hong <hhk7734@gmail.com>
2020-12-01 16:47:29 +0900

    main_window: py: add BAUDRATE to portBaudrateComboBox

--- lib/main_window/main_window.py
+++ lib/main_window/main_window.py
@@ -8,7 +8,32 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
+BAUDRATE = (
+    300,
+    1_200,
+    2_400,
+    4_800,
+    9_600,
+    19_200,
+    38_400,
+    57_600,
+    74_880,
+    115_200,
+    230_400,
+    250_000,
+    460_800,
+    500_000,
+    576_000,
+    921_600,
+    1_000_000,
+    2_000_000,
+)
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        for baudrate in BAUDRATE:
+            self.portBaudrateComboBox.addItem(f"{baudrate}")
+        self.portBaudrateComboBox.setCurrentIndex(BAUDRATE.index(115200))
