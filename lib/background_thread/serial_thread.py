a1cf03014e3154fef46b34279efa69feccdf86dc
Hyeonki Hong <hhk7734@gmail.com>
2020-12-01 21:14:36 +0900

    serial_thread: add default serial functions

--- lib/background_thread/serial_thread.py
+++ lib/background_thread/serial_thread.py
@@ -3,7 +3,7 @@ import os
 import queue
 import serial
 
-from PySide2.QtCore import QThread
+from PySide2.QtCore import QThread, Signal
 
 if os.name == "nt":
     from serial.tools.list_ports_windows import comports
@@ -17,22 +17,80 @@ log.setLevel(logging.DEBUG)
 
 class SerialThread(QThread):
     CMD_TERMINATE = 1
+    CMD_TRANSMIT = 2
+
+    receivedData = Signal(str)
+    disconnectedAbnomally = Signal()
 
     def __init__(self):
         super().__init__()
         self._cmdQueue = queue.Queue()
+        self._uart = serial.Serial()
+        self._uart.timeout = 0  # Non-blocking
 
-    @classmethod
-    def getPorts(cls):
-        return {port: desc for port, desc, _ in comports()}
+    def start(
+        self,
+        port,
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        bytesize=serial.EIGHTBITS,
+        stopbits=serial.STOPBITS_ONE,
+        xonxoff=False,
+        rtscts=False,
+        dsrdtr=False,
+    ):
+        if self.isRunning():
+            raise RuntimeError("Already running.")
+        else:
+            self._uart.port = port
+            self._uart.baudrate = baudrate
+            self._uart.parity = parity
+            self._uart.bytesize = bytesize
+            self._uart.stopbits = stopbits
+            self._uart.xonxoff = xonxoff
+            self._uart.rtscts = rtscts
+            self._uart.dsrdtr = dsrdtr
+            super().start()
 
     def run(self):
+        log.info("SerialThread Starts.")
+        self._uart.open()
+
+        while not self._cmdQueue.empty():
+            self._cmdQueue.get()
+
         runningCondition = True
         while runningCondition:
             if not self._cmdQueue.empty():
                 cmd = self._cmdQueue.get()
                 if cmd[0] == self.CMD_TERMINATE:
                     runningCondition = False
+                elif cmd[0] == self.CMD_TRANSMIT:
+                    self._uart.write(cmd[1].encode())  # str -> bytes
+            try:
+                data = self._uart.read(100)
+            except serial.serialutil.SerialException:
+                self.disconnectedAbnomally.emit()
+                break
+
+            if data != b"":
+                self.receivedData.emit(data.decode())  # bytes -> str
+
+        self._uart.close()
+        log.info("SerialThread is Finished.")
 
     def putCmdQueue(self, *args):
-        self._cmdQueue.put(args)
+        if self.isRunning():
+            self._cmdQueue.put(args)
+        else:
+            raise RuntimeError("Not running.")
+
+    def terminateEventLoop(self):
+        self.putCmdQueue(self.CMD_TERMINATE)
+
+    def transmit(self, data: str):
+        self.putCmdQueue(self.CMD_TRANSMIT, data)
+
+    @classmethod
+    def getPorts(cls):
+        return {port: desc for port, desc, _ in comports()}
