6ffdee348e9478e86dbc03562620dec2f6f5e73d
Hyeonki Hong <hhk7734@gmail.com>
2020-12-02 15:21:50 +0900

    briefcase: init

--- py_src/pyside2_serial/app.py
+++ py_src/pyside2_serial/app.py
@@ -3,14 +3,15 @@ import logging
 
 from PySide2.QtWidgets import QApplication
 
-from lib.main_window.main_window import MainWindow
+from .main_window.main_window import MainWindow
 
 
 logging.basicConfig(
     format="[%(levelname)-8s] %(asctime)s %(threadName)s %(filename)s %(lineno) 4d 행 : %(message)s",
 )
 
-if __name__ == "__main__":
+
+def main():
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
